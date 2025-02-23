2118bd27b5fada5c3648ea7195a3bebceab22ece
lazarevFedor <lazarevfedor@gmail.com>
2025-02-23 13:39:23 +0300

    Add global variables and synchronization primitives for task management

---
+++ orchestrator/internal/entities/globals.go
@@ -0,0 +1,15 @@
+package entities
+
+import (
+	"pkg"
+	"sync"
+)
+
+var (
+	Wg          = &sync.WaitGroup{}
+	ParserMutex = &sync.Mutex{}
+	ParsersTree = pkg.NewRBTree()
+	Tasks       = &pkg.Queue{}
+	Expressions = pkg.NewSafeMap()
+	TasksLastID = &pkg.Counter{}
+)
