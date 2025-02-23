e373c551a69e38e8d1fa5b8e9e879426fd4ecd5c
lazarevFedor <lazarevfedor@gmail.com>
2025-02-23 14:00:27 +0300

    Add timestamp field and getter/setter methods to ClientResponse

--- orchestrator/internal/entities/orchestrator_response.go
+++ orchestrator/internal/entities/orchestrator_response.go
@@ -1,9 +1,22 @@
 package entities
 
+import "time"
+
 // ClientResponse is a struct that contains the response to the client
 type ClientResponse struct {
-	Id     int     `json:"id"`
-	Status string  `json:"status,omitempty"`
-	Result float64 `json:"result,omitempty"`
-	Error  string  `json:"error,omitempty"`
+	Id        int     `json:"id"`
+	Status    string  `json:"status,omitempty"`
+	Result    float64 `json:"result,omitempty"`
+	Error     string  `json:"error,omitempty"`
+	timestamp time.Time
+}
+
+// SetTimestamp sets the timestamp for the ClientResponse
+func (cr *ClientResponse) SetTimestamp(t time.Time) {
+	cr.timestamp = t
+}
+
+// GetTimestamp gets the timestamp for the ClientResponse
+func (cr *ClientResponse) GetTimestamp() time.Time {
+	return cr.timestamp
 }
