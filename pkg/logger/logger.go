e5807bc26dc1b0f27a44af16256d7e3fadfd6149
lazarevFedor <lazarevfedor@gmail.com>
2025-02-27 13:10:20 +0300

    Rename logger context functions for consistency and improved readability

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -9,11 +9,11 @@ type key string
 
 const loggerKey = key("logger")
 
-func withLogger(ctx context.Context, logger *slog.Logger) context.Context {
+func WithLogger(ctx context.Context, logger *slog.Logger) context.Context {
 	return context.WithValue(ctx, loggerKey, logger)
 }
 
-func getLogger(ctx context.Context) *slog.Logger {
+func GetLogger(ctx context.Context) *slog.Logger {
 	logger := ctx.Value(loggerKey)
 	return logger.(*slog.Logger)
 }
