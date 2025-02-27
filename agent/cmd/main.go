05367633ca2194300577669e331acf585c27dae5
lazarevFedor <lazarevfedor@gmail.com>
2025-02-27 13:10:31 +0300

    Refactor main function to integrate context and structured logging for client and server

--- agent/cmd/main.go
+++ agent/cmd/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "agent/internal/client"
+import (
+	"agent/internal/client"
+	"context"
+	"log/slog"
+	"os"
+	"pkg/logger"
+)
 
 func main() {
-	client.ManageTasks()
+	clientLogger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	ctx := logger.WithLogger(context.Background(), clientLogger)
+	client.ManageTasks(ctx)
 }
--- orchestrator/cmd/main.go
+++ orchestrator/cmd/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"context"
+	"log/slog"
 	"orchestrator/internal/server"
+	"os"
+	"pkg/logger"
 )
 
 func main() {
-	//pkg.Init()
-	err := server.StartServer()
+	serverLogger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	ctx := logger.WithLogger(context.Background(), serverLogger)
+	err := server.StartServer(ctx)
 	if err != nil {
 		return
 	}
