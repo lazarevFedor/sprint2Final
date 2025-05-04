821cb36aba6700219d4beacedef47df421508aa8
lazarevFedor <fedos1611@yandex.ru>
2025-05-04 18:35:44 +0300

    client turn to grpc client and communicate with grpc orchestrator

--- agent/internal/client/client.go
+++ agent/internal/client/client.go
@@ -3,105 +3,83 @@ package client
 import (
 	"agent/internal/demon"
 	"agent/internal/entities"
-	"bytes"
 	"context"
-	"encoding/json"
-	"net/http"
 	"os"
+	"pkg/api"
 	logger2 "pkg/logger"
 	"strconv"
 	"time"
 )
 
+type AgentClient struct {
+	client api.OrchestratorClient
+}
+
+func NewAgentClient(client api.OrchestratorClient) *AgentClient {
+	return &AgentClient{client: client}
+}
+
 // ManageTasks is a function that manages tasks
-func ManageTasks(ctx context.Context) {
+func ManageTasks(ctx context.Context, agent *AgentClient) {
 	logger := logger2.GetLogger(ctx)
 	logger.Info("ManageTasks: Start")
-	client := &http.Client{}
 	ticker := time.NewTicker(5 * time.Second)
 	defer ticker.Stop()
-
 	computingPower, err := strconv.Atoi(os.Getenv("COMPUTING_POWER"))
 	if err != nil || computingPower <= 0 {
 		computingPower = 1
 	}
-
 	taskChan := make(chan entities.AgentResponse, 1)
 
 	for i := 0; i < computingPower; i++ {
-		go worker(client, taskChan, ctx)
+		go worker(agent, taskChan, ctx)
 	}
 
 	for range ticker.C {
-		req, err := http.NewRequest("GET", "http://orchestrator:8080/internal/task", nil)
+		taskAccepted, err := agent.client.GetTask(ctx, &api.GetTaskRequest{})
 		if err != nil {
-			logger.Error("ManageTasks: GET request error:", "err", err)
 			continue
 		}
 
-		resp, err := client.Do(req)
-		if err != nil || resp.StatusCode == http.StatusNotFound {
-			continue
-		} else if resp.StatusCode == http.StatusInternalServerError {
-			logger.Error("ManageTasks: getting response error:", "err", err)
-			return
+		task := entities.AgentResponse{
+			Id:            int(taskAccepted.Id),
+			Arg1:          float64(taskAccepted.Arg1),
+			Arg2:          float64(taskAccepted.Arg2),
+			Operation:     taskAccepted.Operation,
+			OperationTime: int(taskAccepted.OperationTime),
 		}
 
-		var task entities.AgentResponse
-		if err := json.NewDecoder(resp.Body).Decode(&task); err != nil {
-			logger.Error("ManageTasks: decoding response error:", "err", err)
-			continue
-		}
-		err = resp.Body.Close()
-		if err != nil {
-			logger.Error("ManageTasks: closing response error:", "err", err)
-			return
-		}
+		logger.Info("ManageTasks: Task accepted:", task.Id)
+
 		taskChan <- task
 		logger.Info("ManageTasks: Task received")
 	}
 }
 
 // worker is a function that processes tasks
-func worker(client *http.Client, taskChan <-chan entities.AgentResponse, ctx context.Context) {
+func worker(agent *AgentClient, taskChan <-chan entities.AgentResponse, ctx context.Context) {
 	for task := range taskChan {
-		solveTask(client, task, ctx)
+		solveTask(agent, task, ctx)
 	}
 }
 
 // solveTask is a function that solves the task
-func solveTask(client *http.Client, task entities.AgentResponse, ctx context.Context) {
+func solveTask(agent *AgentClient, task entities.AgentResponse, ctx context.Context) {
 	logger := logger2.GetLogger(ctx)
 	result, err := demon.CalculateExpression(task.Arg1, task.Arg2, task.Operation, task.OperationTime)
 	if err != nil {
 		logger.Error("solveTask: calculating expression error:", "err", err)
 		return
 	}
-	request := entities.AgentRequest{}
-	request.Id = task.Id
-	request.Result = result
-	data, err := json.Marshal(request)
-	if err != nil {
-		logger.Error("solveTask: marshalling request error:", "err", err)
-		return
-	}
 
-	req, err := http.NewRequest("POST", "http://orchestrator:8080/internal/task", bytes.NewBuffer(data))
+	_, err = agent.client.PostTask(ctx, &api.PostTaskRequest{
+		Id:     int32(task.Id),
+		Result: float32(result),
+	})
 	if err != nil {
-		logger.Error("solveTask: POST request error:", "err", err)
-		return
-	}
-	req.Header.Set("Content-Type", "application/json")
-
-	resp, err := client.Do(req)
-	if err != nil || resp.StatusCode != http.StatusOK {
-		logger.Error("solveTask: sending response error:", "err", err)
-		return
-	}
-	err = resp.Body.Close()
-	if err != nil {
-		logger.Error("solveTask: closing response error:", "err", err)
+		logger.Error("solveTask: post task error:", "err", err)
 		return
 	}
 	logger.Info("solveTask: Task solved")
+	logger.Info("Id:", task.Id, "Result:", result)
 }
