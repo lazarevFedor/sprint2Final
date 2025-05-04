9652365acf6854f67b9fadafff86c4840d5ed6d4
lazarevFedor <fedos1611@yandex.ru>
2025-05-04 18:39:42 +0300

    update for DB and user_id support

--- orchestrator/internal/server/server.go
+++ orchestrator/internal/server/server.go
@@ -2,8 +2,11 @@ package server
 
 import (
 	"context"
+	"database/sql"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"github.com/golang-jwt/jwt/v5"
 	"net/http"
 	obj "orchestrator/internal/entities"
 	"orchestrator/internal/parser"
@@ -14,16 +17,32 @@ import (
 	"time"
 )
 
-// startGarbageCollector starts the garbage collector to remove old tasks
-func startGarbageCollector() {
-	ticker := time.NewTicker(3 * time.Minute)
+const (
+	UpdateExpressionStatus = "UPDATE expressions SET user_id= $1, result = $2, status = $3, expiredIn = $4 WHERE id = $5"
+	DeleteOldExpressions   = "DELETE FROM expressions WHERE status IN ('Done', 'Fail') AND expiredIn < datetime('now', '-4 minutes')"
+	secretKey              = "secret"
+)
+
+// startUpdatingDB updates DB with expressions in map and deletes old expressions
+func startUpdatingDB(ctx context.Context, db *sql.DB) {
+	ticker := time.NewTicker(30 * time.Second)
+	logger := logger2.GetLogger(ctx)
 	go func() {
 		for range ticker.C {
-			now := time.Now()
+			_, err := db.ExecContext(ctx, DeleteOldExpressions)
+			if err != nil {
+				logger.Error("Error deleting old expressions: ", err)
+				return
+			}
 			expressionsMap := obj.Expressions.GetAll()
 			for key, expr := range expressionsMap {
 				task, ok := expr.(obj.ClientResponse)
-				if ok && (task.Status == "Done" || task.Status == "Fail") && now.Sub(task.GetTimestamp()) > 3*time.Minute {
+				if ok && (task.Status == "Done" || task.Status == "Fail") {
+					_, err = db.ExecContext(ctx, UpdateExpressionStatus, task.GetUserId(), task.Result, task.Status, time.Now().UTC().Format("2006-01-02 15:04:05"), task.Id)
+					if err != nil {
+						logger.Error("Error updating expressions: ", err)
+						return
+					}
 					obj.Expressions.Delete(key)
 				}
 			}
@@ -37,61 +56,8 @@ func isValidExpression(expression string) bool {
 	return re.MatchString(expression)
 }
 
-// agentMiddleware is a middleware that handles the agent requests
-func agentMiddleware(getHandler, postHandler http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case http.MethodGet:
-			getHandler.ServeHTTP(w, r)
-		case http.MethodPost:
-			postHandler.ServeHTTP(w, r)
-		default:
-			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		}
-	})
-}
-
-// getHandler handles the /internal/task endpoint
-func getHandler(ctx context.Context) http.HandlerFunc {
-	return func(w http.ResponseWriter, _ *http.Request) {
-		logger := logger2.GetLogger(ctx)
-		if obj.Tasks.IsEmpty() {
-			w.WriteHeader(http.StatusNotFound)
-			return
-		}
-		task := obj.Tasks.Dequeue()
-		w.Header().Set("Content-Type", "application/json")
-		if err := json.NewEncoder(w).Encode(task); err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			logger.Error("getHandler: could not encode response:", "err", err)
-			return
-		}
-	}
-}
-
-// postHandler handles the /internal/task endpoint
-func postHandler(ctx context.Context) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		logger := logger2.GetLogger(ctx)
-		var taskRes obj.ClientResponse
-		if err := json.NewDecoder(r.Body).Decode(&taskRes); err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			logger.Error("postHandler: could not decode request:", "err", err)
-			return
-		}
-		node := obj.ParsersTree.Search(taskRes.Id)
-		if node == nil {
-			w.WriteHeader(http.StatusNotFound)
-			return
-		}
-		ch := node.Value.(*chan float64)
-		*ch <- taskRes.Result
-		w.WriteHeader(http.StatusOK)
-	}
-}
-
 // calculateHandler handles the /api/v1/calculate endpoint
-func calculateHandler(ctx context.Context) http.HandlerFunc {
+func calculateHandler(ctx context.Context, db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		logger := logger2.GetLogger(ctx)
 		var clientRequest obj.ClientRequest
@@ -108,6 +74,12 @@ func calculateHandler(ctx context.Context) http.HandlerFunc {
 			w.WriteHeader(http.StatusUnprocessableEntity)
 			return
 		}
+		userId, ok := r.Context().Value("user_id").(int)
+		if !ok {
+			logger.Warn("calculateHandler: could not get user_id from context")
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
 		clientResponse.Id = obj.TasksLastID.GetValue()
 		w.WriteHeader(http.StatusCreated)
 		w.Header().Set("Content-Type", "application/json")
@@ -118,38 +90,94 @@ func calculateHandler(ctx context.Context) http.HandlerFunc {
 		}
 		obj.TasksLastID.Increment()
 		obj.Wg.Add(1)
-		go parser.Parse(clientRequest.Expression, clientResponse.Id)
-		logger.Info("calculateHandler: expression %s with id %d was added to the queue", clientRequest.Expression, clientResponse.Id)
-	}
-}
-
-func expressionHandler(ctx context.Context) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		logger := logger2.GetLogger(ctx)
-		expressionsMap := obj.Expressions.GetAll()
-		expressions := make([]obj.ClientResponse, 0, len(expressionsMap))
-
-		for _, expr := range expressionsMap {
-			task, ok := expr.(obj.ClientResponse)
-			if !ok {
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-			expressions = append(expressions, task)
-		}
-		w.WriteHeader(http.StatusOK)
-		w.Header().Set("Content-Type", "application/json")
-		if err := json.NewEncoder(w).Encode(map[string]interface{}{"expressions": expressions}); err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			logger.Error("expressionHandler: could not encode response:", "err", err)
+		_, err = db.ExecContext(ctx, "INSERT INTO expressions(id, user_id, expression, status) VALUES(?, ?, ?, ?)", clientResponse.Id, userId, clientRequest.Expression, "In progress")
+		if err != nil {
+			logger.Warn("calculateHandler: could not insert expressions: ", "err", err)
+			obj.Wg.Done()
 			return
 		}
+		go parser.Parse(clientRequest.Expression, clientResponse.Id, userId)
+
+		logger.Info("calculateHandler: expression was added to the queue:", clientResponse.Id)
 	}
 }
 
-func expressionIDHandler(ctx context.Context) http.HandlerFunc {
+// expressionHandler handles the /api/v1/expressions endpoint
+func expressionHandler(ctx context.Context, db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		logger := logger2.GetLogger(ctx)
+		userID, ok := r.Context().Value("user_id").(int)
+		if !ok {
+			logger.Warn("calculateHandler: could not get user_id from context")
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		rows, err := db.QueryContext(ctx, `
+            SELECT id, result, status 
+            FROM expressions 
+            WHERE user_id = ?`,
+			userID,
+		)
+		if err != nil && !errors.Is(err, sql.ErrNoRows) {
+			logger.Error("database query error", "error", err)
+			http.Error(w, `{"error": "internal server error"}`, http.StatusInternalServerError)
+			return
+		}
+		defer func(rows *sql.Rows) {
+			err := rows.Close()
+			if err != nil {
+				logger.Error("database close error", "error", err)
+				return
+			}
+		}(rows)
+
+		var expressions []obj.ClientResponse
+		for rows.Next() {
+			var expr obj.ClientResponse
+
+			err := rows.Scan(
+				&expr.Id,
+				&expr.Result,
+				&expr.Status,
+			)
+
+			if err != nil {
+				logger.Error("row scan error", "error", err)
+				continue
+			}
+
+			expressions = append(expressions, expr)
+		}
+
+		if err = rows.Err(); err != nil {
+			logger.Error("rows iteration error", "error", err)
+			http.Error(w, `{"error": "internal server error"}`, http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+
+		response := map[string]interface{}{
+			"expressions": expressions,
+		}
+
+		if err := json.NewEncoder(w).Encode(response); err != nil {
+			logger.Error("JSON encode error", "error", err)
+		}
+	}
+}
+
+// expressionIDHandler handles the /api/v1/expressions/{id} endpoint
+func expressionIDHandler(ctx context.Context, db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		logger := logger2.GetLogger(ctx)
+		userID, ok := r.Context().Value("user_id").(int)
+		if !ok {
+			logger.Warn("calculateHandler: could not get user_id from context")
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
 		urlParts := strings.Split(r.URL.Path, "/")
 		idStr := urlParts[len(urlParts)-1]
 		id, err := strconv.Atoi(idStr)
@@ -157,15 +185,22 @@ func expressionIDHandler(ctx context.Context) http.HandlerFunc {
 			http.Error(w, "Invalid ID", http.StatusInternalServerError)
 			return
 		}
-
-		expression := obj.Expressions.Get(strconv.Itoa(id))
-		if expression == nil {
-			http.Error(w, "Expression not found", http.StatusNotFound)
+		var expr obj.ClientResponse
+		row := db.QueryRow(`
+            SELECT result, status 
+            FROM expressions 
+            WHERE user_id = ? and id = ?`,
+			userID, id,
+		)
+		err = row.Scan(&expr.Result, &expr.Status)
+		if err != nil && !errors.Is(err, sql.ErrNoRows) {
+			logger.Error("database query error", "error", err)
 			return
 		}
+		expr.Id = id
 		w.WriteHeader(http.StatusOK)
 		w.Header().Set("Content-Type", "application/json")
-		if err = json.NewEncoder(w).Encode(expression); err != nil {
+		if err = json.NewEncoder(w).Encode(expr); err != nil {
 			http.Error(w, "Internal server error", http.StatusInternalServerError)
 			logger.Error("expressionIDHandler: could not encode response:", "err", err)
 			return
@@ -173,22 +208,158 @@ func expressionIDHandler(ctx context.Context) http.HandlerFunc {
 	}
 }
 
-// StartServer starts the server on port 8080 and listens for incoming requests
-func StartServer(ctx context.Context) error {
-	// Get the logger from the context
+// authMiddleware checks auth status of user by jwt token
+func authMiddleware(ctx context.Context) func(http.HandlerFunc) http.HandlerFunc {
+	return func(next http.HandlerFunc) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+
+			authHeader := r.Header.Get("Authorization")
+			if authHeader == "" {
+				sendJSONError(w, "Authorization header required", http.StatusUnauthorized, ctx)
+				return
+			}
+
+			tokenParts := strings.Split(authHeader, " ")
+			if len(tokenParts) != 2 || tokenParts[0] != "Bearer" {
+				sendJSONError(w, "Invalid token format", http.StatusUnauthorized, ctx)
+				return
+			}
+			tokenStr := tokenParts[1]
+
+			token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+				if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+					return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+				}
+				return []byte(secretKey), nil
+			})
+
+			if err != nil || !token.Valid {
+				sendJSONError(w, "Invalid token", http.StatusUnauthorized, ctx)
+				return
+			}
+
+			claims, ok := token.Claims.(jwt.MapClaims)
+			if !ok {
+				sendJSONError(w, "Invalid token claims", http.StatusUnauthorized, ctx)
+				return
+			}
+
+			userID, ok := claims["user_id"].(float64)
+			if !ok || userID == 0 {
+				sendJSONError(w, "Invalid user ID", http.StatusUnauthorized, ctx)
+				return
+			}
+
+			ctx = context.WithValue(ctx, "user_id", int(userID))
+			next(w, r.WithContext(ctx))
+		}
+	}
+}
+
+// sendJSONError sends errors by json in authMiddleware func
+func sendJSONError(w http.ResponseWriter, message string, code int, ctx context.Context) {
 	logger := logger2.GetLogger(ctx)
-	// Set the environment variables
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	err := json.NewEncoder(w).Encode(map[string]string{
+		"error":   http.StatusText(code),
+		"message": message,
+	})
+	if err != nil {
+		logger.Error("JSON encode error", "error", err)
+		return
+	}
+	logger.Error("JSON encode error", "error", message)
+}
+
+// registerHandler handles the /api/v1/register endpoint
+func registerHandler(ctx context.Context, db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		logger := logger2.GetLogger(ctx)
+		var user obj.RegisterRequest
+		err := json.NewDecoder(r.Body).Decode(&user)
+		if err != nil {
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			logger.Error("registerHandler: could not decode request:", "err", err)
+			return
+		}
+
+		_, err = db.ExecContext(ctx, "INSERT INTO users (login, password) VALUES (?, ?)", user.Login, user.Password)
+		if err != nil {
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			logger.Error("registerHandler: could not insert user:", "err", err)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+	}
+}
+
+// GenerateToken generates jwt token
+func GenerateToken(userID int, secretKey string) (string, error) {
+	claims := jwt.MapClaims{
+		"user_id": userID,
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString([]byte(secretKey))
+	if err != nil {
+		return "", fmt.Errorf("failed to sign token: %v", err)
+	}
+	return tokenString, nil
+}
+
+// loginHandler handles the /api/v1/login endpoint
+func loginHandler(ctx context.Context, db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		logger := logger2.GetLogger(ctx)
+		var user obj.LoginRequest
+		var id int
+		err := json.NewDecoder(r.Body).Decode(&user)
+		if err != nil {
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			logger.Error("loginHandler: could not decode request:", "err", err)
+			return
+		}
+		row := db.QueryRow("SELECT id FROM users WHERE login = ? AND password = ?", user.Login, user.Password)
+		err = row.Scan(&id)
+		if err != nil {
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			logger.Error("loginHandler: could not find user:", "err", err)
+			return
+		}
+
+		token, err := GenerateToken(id, secretKey)
+		if err != nil {
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			logger.Error("loginHandler: could not generate token:", "err", err)
+			return
+		}
+
+		resp := obj.LoginResponse{
+			Token: token,
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		if err := json.NewEncoder(w).Encode(resp); err != nil {
+			logger.Error("loginHandler: could not encode response:", "err", err)
+			return
+		}
+	}
+}
+
+// StartServer starts the server on port 8080 and listens for incoming requests
+func StartServer(ctx context.Context, db *sql.DB) error {
+	logger := logger2.GetLogger(ctx)
 
 	mux := http.NewServeMux()
-	// Start the garbage collector to remove old tasks
-	startGarbageCollector()
+	//start updating DB
+	startUpdatingDB(ctx, db)
 	// Handle functions for client requests
-	mux.HandleFunc("/api/v1/calculate", calculateHandler(ctx))
-	mux.HandleFunc("/api/v1/expressions", expressionHandler(ctx))
-	mux.HandleFunc("/api/v1/expressions/", expressionIDHandler(ctx))
-
-	// Handle functions for agent requests
-	mux.Handle("/internal/task", agentMiddleware(getHandler(ctx), postHandler(ctx)))
+	mux.HandleFunc("/api/v1/register", registerHandler(ctx, db))
+	mux.HandleFunc("/api/v1/login", loginHandler(ctx, db))
+	mux.HandleFunc("/api/v1/calculate", authMiddleware(ctx)(calculateHandler(ctx, db)))
+	mux.HandleFunc("/api/v1/expressions", authMiddleware(ctx)(expressionHandler(ctx, db)))
+	mux.HandleFunc("/api/v1/expressions/", authMiddleware(ctx)(expressionIDHandler(ctx, db)))
 
 	// Start the server
 	err := http.ListenAndServe(":8080", mux)
