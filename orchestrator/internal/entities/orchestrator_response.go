59c7d9e46611c953d8741d526ca464c00fa3e57d
lazarevFedor <fedos1611@yandex.ru>
2025-05-05 19:03:15 +0300

    rm: timestamp

--- orchestrator/internal/entities/orchestrator_response.go
+++ orchestrator/internal/entities/orchestrator_response.go
@@ -1,29 +1,16 @@
 package entities
 
-import "time"
-
 // ClientResponse is a struct that contains the response to the client
 type ClientResponse struct {
-	userId    int
-	Id        int     `json:"id"`
-	Status    string  `json:"status,omitempty"`
-	Result    float64 `json:"result,omitempty"`
-	Error     string  `json:"error,omitempty"`
-	timestamp time.Time
+	userId int
+	Id     int     `json:"id"`
+	Status string  `json:"status,omitempty"`
+	Result float64 `json:"result,omitempty"`
+	Error  string  `json:"error,omitempty"`
 }
 
 type LoginResponse struct {
 	Token string
-}
-
-// SetTimestamp sets the timestamp for the ClientResponse
-func (cr *ClientResponse) SetTimestamp(t time.Time) {
-	cr.timestamp = t
-}
-
-// GetTimestamp gets the timestamp for the ClientResponse
-func (cr *ClientResponse) GetTimestamp() time.Time {
-	return cr.timestamp
 }
 
 func (cr *ClientResponse) GetUserId() int {
